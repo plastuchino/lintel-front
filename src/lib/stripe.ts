d8db542d00279088f62d368ed4bda9217f9e93d9
Sebastian <maracuchoamericano@gmail.com>
2026-05-27 23:34:22 -0400

    people couldnt pay in prod

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,3 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripeKey = window.location.hostname === 'localhost'
-  ? import.meta.env.VITE_STRIPE_PUBLIC_KEY
-  : import.meta.env.VITE_STRIPE_PUBLIC_KEY_PROD;
-
-export const stripePromise = loadStripe(stripeKey ?? '');
+export const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY ?? '');
